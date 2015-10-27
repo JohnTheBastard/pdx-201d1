ee525df76d97afeebdc6291d34c01a13c3368f01
John Hearn <jhearn@gmail.com>
2015-10-27 16:29:20 -0700

    cleaned up some style/formatting issues

--- guessing-game/game.js
+++ guessing-game/game.js
@@ -20,7 +20,7 @@ var questions = [ { query      : "Am I from Texas?",
 var score = 0
 var userResponses = [];
 var userMistakes  = [];
-var userName = prompt("What's you name?");
+var userName = prompt( "What's you name?" );
 
 // Normalize responses to YES or NO based on the first letter 
 // of the string, or returns the boolean 'false' if the input
@@ -37,32 +37,32 @@ var normalize = function(response) {
 
 
 /*  Get responses from user */
-for ( i=0; i < questions.length; i++) {
-    var input = normalize(prompt(questions[i].query));
+for ( i=0; i < questions.length; i++ ) {
+    var input = normalize( prompt( questions[i].query ) );
 
     // Keep trying til you get a YES or NO.
-    while (!input) {
+    while ( !input ) {
 
 	var errorMessage = "I'm sorry, I didn't get that. " + 
 	                   "Please answer YES or NO. \n\n"
-	input = normalize(prompt(errorMessage + questions[i].query));	
+	input = normalize( prompt( errorMessage + questions[i].query ) );	
     }
     
-    userResponses.push(input);
+    userResponses.push( input );
 
-    if ( userResponses[i] === questions[i].answer) {
+    if ( userResponses[i] === questions[i].answer ) {
 	score++;
-	alert("You answered " + userResponses[i] + "\n" + 
-	      "Good job, " + userName + ". That's correct! "
-	      + questions[i].correction);
+	alert( "You answered " + userResponses[i] + "\n" + 
+	       "Good job, " + userName + ". That's correct! "
+	       + questions[i].correction );
     } else {
 	userMistakes.push(i);
-	alert("You answered " + userResponses[i] + "\n" + 
-	      "I'm sorry, " + userName + ". That's not right. "
-	      + questions[i].correction);
+	alert( "You answered " + userResponses[i] + "\n" + 
+	       "I'm sorry, " + userName + ". That's not right. "
+	       + questions[i].correction );
     }
 }
 
 /* Tell user how they did */
-alert(userName + ", you scored " + score + " out of "
-       + questions.length + ".\n" + "Thanks for playing!");
+alert( userName + ", you scored " + score + " out of "
+       + questions.length + ".\n" + "Thanks for playing!" );
