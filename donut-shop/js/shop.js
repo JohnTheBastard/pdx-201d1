e1994db700e44b5d4e31d454b75343607438913f
John Hearn <jhearn@gmail.com>
2015-11-06 14:34:33 -0800

    donut shop: code clean up and added sort method

--- donut-shop/js/shop.js
+++ donut-shop/js/shop.js
@@ -24,7 +24,7 @@ var defaultInput = [ [ "Downtown",         8, 43, 4.50 ],
      * * * * * * CONSTRUCTORS  * * * * * *
      * * * * * * * * * * * * * * * * * * */    
 
-function Franchise(location, minimum, maximum, average) {
+function Franchise( location, minimum, maximum, average ) {
     this.location = location;
     this.minCPH = minimum;
     this.maxCPH = maximum;
@@ -37,19 +37,19 @@ function Franchise(location, minimum, maximum, average) {
     }
     
     this.hourOfDonutSales = function() {
-	return Math.ceil(this.avgDonutsPC * this.randomizedCustomerVolume() );
+	return Math.ceil( this.avgDonutsPC * this.randomizedCustomerVolume() );
     }
 }
 
 function Row() {
-    this.element = document.createElement("tr");
-    this.addCell = function(tag, myText) {
+    this.element = document.createElement( "tr" );
+    this.addCell = function( tag, myText ) {
 	var cell = document.createElement( tag );
 	var text = document.createTextNode( myText );
 	cell.appendChild( text );
 	this.element.appendChild( cell );
     }
-    this.addHeadCells = function (headData ) {
+    this.addHeadCells = function( headData ) {
 	for (var ii=0; ii < headData.length; ii++) {
 	    this.addCell( "th", headData[ii] );
 	}
@@ -59,7 +59,7 @@ function Row() {
 	var hourOfSales = 0;
 	var salesTotal = 0;
 	this.addCell( tag, myFranchise.location );
-	for (var jj=0; jj < 11; jj++) {
+	for ( var jj=0; jj < 11; jj++ ) {
 	    hourOfSales = myFranchise.hourOfDonutSales();
 	    salesTotal += hourOfSales;
 	    this.addCell(tag, hourOfSales);
@@ -68,93 +68,54 @@ function Row() {
     }
 }
 
-
-function SalesTable(headData, rowData) {
+function SalesTable( headData, rowData ) {
     this.table = document.createElement( "table" );
     this.init = function init() {
-	this.addHead(headData);
+	this.addHead( headData );
 	for ( var ii = 0; ii < rowData.length; ii++ ) {
 	    this.addRow( rowData[ii] );
 	}
     }
-    this.addHead = function () {
+    this.addHead = function() {
 	var row = new Row();
-	row.addHeadCells(headData);
-	return this.table.appendChild(row.element);
+	row.addHeadCells( headData );
+	return this.table.appendChild( row.element );
     }
-    this.addRow = function(franchise) {
+    this.addRow = function( franchise ) {
 	var row = new Row();
-	row.addRowCells(franchise);
-	return this.table.appendChild(row.element);
+	row.addRowCells( franchise );
+	return this.table.appendChild( row.element );
     }
 }
-
 
 var DONUT_MODULE = (function() {
     // maybe some day
     //'use strict';
-    
-    var my = { };
-    my.franchises = [ ];
-    my.attachmentNode = document.getElementById("tableNode");
-    
-    
-    /* * * * * * * * * * * * * * * * * * * *
-     * * * * PRIVATE MODULE METHODS  * * * *
-     * * * * * * * * * * * * * * * * * * * */
 
-    // I'm trying to make these "private" methods,
-    // but this may be an anti-pattern.
+    var my = { };
+    my.attachmentNode = document.getElementById( "moduleNode" );
+    my.franchises = [ ];
+
     
-    var pushFranchise = function(franchise) {
-	if (my.franchises.doesntHave(franchise)) {
-	    my.franchises.push(franchise);
-	} else {
-	    var oldIndex = my.franchises.getIndex(franchise);
-	    my.franchises[oldIndex].minCPH = franchise.minCPH;
-	    my.franchises[oldIndex].maxCPH = franchise.maxCPH;
-	    my.franchises[oldIndex].avgDonutsPC = franchise.avgDonutsPC;
-	}
-    }
-    
-    var initFranchiseArray = function( initData ) {
-	//var franchiseArray = [ ];
+    /* * * * * * * * * * * * * * * * * * *
+     * * * * PUBLIC MODULE METHODS * * * *
+     * * * * * * * * * * * * * * * * * * */
+
+    my.franchises.init = function( initData ) {
 	for ( var ii=0; ii < initData.length; ii++ ) {
 	    var franchise = new Franchise( initData[ii][0],
 					   initData[ii][1],
 					   initData[ii][2],
 					   initData[ii][3] );
-	    pushFranchise(franchise);
+	    my.franchises.push( franchise );
+	    my.franchises.sort( my.compare );
 	}
-	
     }
-
-    /* * * * * * * * * * * * * * * * * * *
-     * * * * PUBLIC MODULE METHODS * * * *
-     * * * * * * * * * * * * * * * * * * */
-
-    // This gets called from HTML
-    my.addUserInput = function() {
-	var loc = document.getElementById('location').value; 
-	var min = parseInt( document.getElementById('minCPH').value, 10 ); 
-	var max = parseInt( document.getElementById('maxCPH').value, 10 ); 
-	var avg = parseFloat( document.getElementById('avgDPC').value );
-	var userFranchise = new Franchise(loc, min, max, avg);
-
-	// store (or update) our new franchise data
-	pushFranchise(userFranchise);
-
-	// redraw the table
-	my.attachmentNode.removeChild(my.sales.table);
-	my.sales = new SalesTable(headStrings, my.franchises);
-	my.sales.init();
-	my.attachmentNode.appendChild(my.sales.table);
-    }
-
-    my.franchises.doesntHave = function(franchise) {
+    
+    my.franchises.doesntHave = function( franchise ) {
 	// Make sure our franchise location doesn't already exist.
 	// (I hate doing it like this, but JavaScript objects suck.)
-	for (var ii=0; ii < my.franchises.length; ii++) {
+	for ( var ii=0; ii < my.franchises.length; ii++ ) {
 	    if ( franchise.location == my.franchises[ii].location ) {
 		return false;
 	    }
@@ -162,31 +123,62 @@ var DONUT_MODULE = (function() {
 	return true;
     }
 
-    my.franchises.getIndex = function(franchise) {
-	for (var ii=0; ii < my.franchises.length; ii++) {
+    my.franchises.getIndex = function( franchise ) {
+	for ( var ii=0; ii < my.franchises.length; ii++ ) {
 	    if ( franchise.location == my.franchises[ii].location ) {
 		return ii;
 	    }
 	}
     }
-
     
+    // Override default array push() method to prevent duplicates
+    my.franchises.push = function( franchise ) {
+	if ( my.franchises.doesntHave( franchise ) ) {
+	    my.franchises[ my.franchises.length ] = franchise;
+	    my.franchises.sort( my.compare );
+	} else {
+	    var oldIndex = my.franchises.getIndex( franchise );
+	    my.franchises[ oldIndex ].minCPH = franchise.minCPH;
+	    my.franchises[ oldIndex ].maxCPH = franchise.maxCPH;
+	    my.franchises[ oldIndex ].avgDonutsPC = franchise.avgDonutsPC;
+	}
+    }
 
-
+    my.compare = function compare( franchise1, franchise2 ) {
+	if ( franchise1.location < franchise2.location )
+	    return -1;
+	if ( franchise1.location > franchise2.location )
+	    return 1;
+	return 0;
+    }
     
+    // This gets called from HTML
+    my.addUserInput = function() {
+	var loc = document.getElementById( 'location' ).value; 
+	var min = parseInt( document.getElementById( 'minCPH' ).value, 10 ); 
+	var max = parseInt( document.getElementById( 'maxCPH' ).value, 10 ); 
+	var avg = parseFloat( document.getElementById( 'avgDPC' ).value );
+	var userFranchise = new Franchise( loc, min, max, avg );
+
+	// store (or update) our new franchise data
+	my.franchises.push( userFranchise );
+
+	// redraw the table
+	my.attachmentNode.removeChild( my.sales.table );
+	my.sales = new SalesTable( headStrings, my.franchises );
+	my.sales.init();
+	my.attachmentNode.appendChild( my.sales.table );
+    }
+
     /*********************************** 
      ***** Stuff Actually Happens ******
      ***********************************/
-    
-    initFranchiseArray(defaultInput);
-    
-    my.sales = new SalesTable(headStrings, my.franchises);
+
+    my.franchises.init( defaultInput );
+    my.sales = new SalesTable( headStrings, my.franchises );
     my.sales.init();
-    my.attachmentNode.appendChild(my.sales.table);
+    my.attachmentNode.appendChild( my.sales.table );
 
     return my;
     
 })();
-
-
-
